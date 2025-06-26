2d8dac83febd26447e9845678438596157e5acad
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-26 15:13:46 -0400

    3_deck.js practice complete

--- 3_deck.js
+++ 3_deck.js
@@ -78,9 +78,13 @@ const createDeck = () => {
 }
 
 // 6. Call the createDeck() function and store the results in a new variable named myDeck
+let myDeck = createDeck()
 
 /* 7. Use a for loop to loop through the deck and list each card in the order the appear in the newly shuffled array. Use the log() method to print out a message like this, once for each card:
 "7 of ♥.︎"
 */
+for (let i = 0; i < myDeck.length; i++) {
+  console.log(`${myDeck[i][0]} of ${myDeck[i][1]}.`)
+}
 
 // Run your code by typing node 3_deck.js in the console below
